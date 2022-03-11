292bca70fc6ad34683b8bee4fd2e741da78ee7a2
sammi-njine <99355233+sammi-njine@users.noreply.github.com>
2022-03-10 20:13:15 -0800

    using puts to output to std out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
  * main - entry point
+ *
  * receives var of type char and prints that out to std out with puts
- * return - always 0
+ *
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	char firstc[] = "Programming is like building a multilingual puzzle";
+	char firstc[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(firstc);
 
