9bb8456a9152c9ba0c4c0a02d7a598d7b3c18ae0
sammi-njine <99355233+sammi-njine@users.noreply.github.com>
2022-03-10 20:23:51 -0800

    print to stdout with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * use printf to output to stdio
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
