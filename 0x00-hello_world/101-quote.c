1ee2f3fda6541cbb685ad5a5ef71c8c6606f9bb4
sammi-njine <99355233+sammi-njine@users.noreply.github.com>
2022-03-11 18:50:13 -0800

    if else positives and negatives

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * printing to stderr
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fwrite(stderr, "and that piece of art is useful\' \- Dora Korpar\, 2015-10-19\n");
+
+	return (1);
+}
