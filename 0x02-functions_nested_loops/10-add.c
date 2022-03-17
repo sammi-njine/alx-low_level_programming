26f6419c662819305cf4bd827c5ebf9b73ba11c3
sammi-njine <99355233+sammi-njine@users.noreply.github.com>
2022-03-17 02:41:53 +0000

    sum of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @x: first value
+ * @y: second value
+ *
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	sum = x + y;
+
+	return (sum);
+
+}
