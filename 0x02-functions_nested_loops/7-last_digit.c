00500c960386844964dbc18ef5619480fc177760
sammi-njine <99355233+sammi-njine@users.noreply.github.com>
2022-03-17 02:05:06 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check what the last digit of a number is
+ *
+ * @n: number being checked
+ *
+ * Return: n which is the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int x = n % 10;
+
+	if (x < 0)
+		x *= -1;
+
+	_putchar (x + '0');
+		return (n);
+}
